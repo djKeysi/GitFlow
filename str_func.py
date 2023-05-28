848429a1e2fe83c62806025ac2021ad89760eb30
evgeniy <evgeniyzenchenko@gmail.com>
2023-05-28 04:51:11 +0200

    Add str_title

--- str_func.py
+++ str_func.py
@@ -2,3 +2,7 @@ def str_upper(str):
     """Функция делает заглавными буквами"""
     return str.upper()
 
+def str_title(str):
+    """функция делает первую букву заглавной"""
+    return str.title()
+
