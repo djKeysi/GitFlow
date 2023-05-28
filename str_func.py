421b053b41cf6a7c59755894b3d30ff02e4324e9
djKeysi <100343540+djKeysi@users.noreply.github.com>
2023-05-28 10:22:06 +0700

    Update2 str_func.py

--- str_func.py
+++ str_func.py
@@ -2,9 +2,3 @@ def str_upper(str):
     """Функция делает заглавными буквами"""
     return str.upper()
 
-def str_title(str):
-    """функция делает первую букву заглавнойPASS"""
-    return str.title()
-
-def test():
-    pass
